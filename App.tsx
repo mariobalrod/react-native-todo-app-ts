078d6d899f11980d590df4cbdfa6bb422cb17f7c
mariobalrod <marioballestero.rdg@gmail.com>
2020-09-06 19:36:46 +0200

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Welcome to my</Text>
+      <Text>First React Native App</Text>
+      <Button
+        onPress={() => console.log('Join')}
+        title="Join"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +24,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
